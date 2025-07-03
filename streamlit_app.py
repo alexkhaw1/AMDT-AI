1fc113127b8d8cbd5b0a1e7a5782d1297b3be8fe
alexkhaw1 <68465770+alexkhaw1@users.noreply.github.com>
2025-07-03 11:51:03 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title ('Dell Global Business Center')
+#st.image('Screenshot 2024-07-16 094056.jpg')
+st.button('Welcome')
+
+st.date_input("Trasaction Date")
+st.radio("your department name:",['DAA','NPI','GMOT'])
+#st.camera_input("Case Reported")
